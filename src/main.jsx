f2234adb7ebc1627132173c73db6f7b99153f70f
Sohail Ahmad <sohail.bsse01@gmail.com>
2025-02-07 02:49:45 +0000

    react router added with theme setting

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from "react-router";
+import SignIn from "@/auth/sign-in/index.jsx";
+
+const router = createBrowserRouter(
+    [
+        {
+            path: '/',
+            element: <App/>
+        },
+        {
+            path: '/auth/sign-in',
+            element: <SignIn/>
+        }
+    ]
+)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <RouterProvider router={router}/>
+    </StrictMode>,
 )
