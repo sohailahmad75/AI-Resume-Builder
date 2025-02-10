a1ad0e2ace8782d81fad98f55a18b8715ef41d8b
Sohail Ahmad <sohail.bsse01@gmail.com>
2025-02-10 14:51:33 +0000

    theme fixes and dialog added for dashboard

---
+++ src/dashboard/components/AddResume.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { PlusSquare } from "lucide-react";
+
+const AddResume = () => {
+  return (
+    <div>
+      <div
+        className="p-14 py-24 border items-center flex
+      justify-center bg-secondary rounded-lg h-[280px]
+      hover:scale-105 transition-all hover:shadow cursor-pointer border-dashed"
+      >
+        <PlusSquare />
+      </div>
+    </div>
+  );
+};
+
+export default AddResume;
--- src/dashboard/index.jsx
+++ src/dashboard/index.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import AddResume from "@/dashboard/components/AddResume.jsx";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 const Dashboard = () => {
-  return <div>dashboard</div>;
+  return (
+    <div className="p-10 md:px-20 lg:px-32">
+      <h2 className="font-blod text-3xl">My Resume</h2>
+      <p>
+        Start Creating A Resume to your next job with our easy to use resume
+        builder.
+      </p>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 mt-10">
+        <AddResume />
+      </div>
+
+      <Dialog>
+        <DialogTrigger>Open</DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
 };
 
 export default Dashboard;
